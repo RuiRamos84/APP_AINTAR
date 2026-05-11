612d82e24c85ded21bd360430a5091461ebf3312
Rui Ramos <estgv18546@alunos.estgv.ipv.pt>
2026-05-11 16:11:25 +0100

    feat(version): atualizar versão e build para 3.6.1 e 87, respectivamente

--- backend/app/__version__.py
+++ backend/app/__version__.py
@@ -1,4 +1,4 @@
 # Auto-gerado pelo sistema de deploy -- nao editar manualmente
-__version__ = "3.5.0"
-__build__ = 80
-__deploy_date__ = "2026-05-09 23:32:08"
+__version__ = "3.6.1"
+__build__ = 87
+__deploy_date__ = "2026-05-11 15:35:06"
